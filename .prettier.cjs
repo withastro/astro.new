21e260a70aa8cc319ef0721415ecc7104880f4b5
calebjasik <calebjasik@jasik.xyz>
2022-09-23 14:51:00 -0500

    `npm run format` + add prettier and other packages

---
+++ .prettier.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  plugins: [
+    require("prettier-plugin-astro"),
+    require("prettier-plugin-tailwindcss"),
+  ],
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: true,
+  overrides: [
+    {
+      files: [".*", "*.json", "*.md", "*.toml", "*.yml"],
+      options: {
+        useTabs: false,
+      },
+    },
+    {
+      files: ["**/*.astro"],
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
