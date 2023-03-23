e0c67246f56e04da0cf37dff55e5572a940dcc5f
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-03-23 12:53:16 -0500

    tooling updates

--- tests/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("basic test", async ({ page }) => {
-  await page.goto("/");
-  const title = page.locator("main h1");
-  await expect(title).toHaveText("astro.new");
-});
