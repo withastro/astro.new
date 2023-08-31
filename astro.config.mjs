d79338e63a994b6e8a4dd1c7eb1eef099d14cecb
Fred K. Schott <fkschott@gmail.com>
2023-08-31 06:14:42 -0700

    fix OG image (#52)
    * fix OG image
    
    * adding support for PR preview URLs in SEO images
    
    ---------
    
    Co-authored-by: Tony Sullivan <tony.f.sullivan@outlook.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@ import netlify from "@astrojs/netlify/functions"
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 
+/* https://docs.netlify.com/configure-builds/environment-variables/#read-only-variables */
+const NETLIFY_PREVIEW_SITE =
+	process.env.CONTEXT !== "production" && process.env.DEPLOY_PRIME_URL
+
 // https://astro.build/config
 export default defineConfig({
+	site: NETLIFY_PREVIEW_SITE || "https://astro.new",
 	integrations: [
 		tailwind({
 			config: {
