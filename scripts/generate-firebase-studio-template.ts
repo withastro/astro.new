2394fdbf644bfeae4d4f991159971eab1ded08c9
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-12-22 14:44:41 +0000

    Apply automatic changes

--- scripts/generate-firebase-studio-template.ts
+++ scripts/generate-firebase-studio-template.ts
@@ -1,31 +1,32 @@
-import { writeFile } from 'fs/promises';
-import { getFirebaseStudioParams } from '../src/data/examples-shared.js';
+import { writeFile } from "fs/promises";
+import { getFirebaseStudioParams } from "../src/data/examples-shared.js";
 
-await generateFirebaseStudioTemplate('latest');
-await generateFirebaseStudioTemplate('next');
+await generateFirebaseStudioTemplate("latest");
+await generateFirebaseStudioTemplate("next");
 
-async function generateFirebaseStudioTemplate(ref: 'latest' | 'next') {
-	console.time(`Updated ${ref}/idx-template.json`);
-	// .idx-templates/latest/idx-template.json
-	const template = {
-		name: ref === 'next' ? 'astro.new/next' : 'astro.new',
-		description: 'Kick start your next project with Astro’s official starter templates',
-		icon: 'https://raw.githubusercontent.com/withastro/astro.build/refs/heads/main/public/assets/press/astro-icon-light-gradient.svg',
-		params: [
-			{
-				id: 'astroTemplate',
-				name: 'Starter template',
-				type: 'enum',
-				default: 'basics',
-				options: await getFirebaseStudioParams(ref),
-				required: true,
-			},
-		],
-	};
-	await writeFile(
-		`./.idx-templates/${ref}/idx-template.json`,
-		JSON.stringify(template, null, 2) + '\n',
-		'utf-8',
-	);
-	console.timeEnd(`Updated ${ref}/idx-template.json`);
+async function generateFirebaseStudioTemplate(ref: "latest" | "next") {
+  console.time(`Updated ${ref}/idx-template.json`);
+  // .idx-templates/latest/idx-template.json
+  const template = {
+    name: ref === "next" ? "astro.new/next" : "astro.new",
+    description:
+      "Kick start your next project with Astro’s official starter templates",
+    icon: "https://raw.githubusercontent.com/withastro/astro.build/refs/heads/main/public/assets/press/astro-icon-light-gradient.svg",
+    params: [
+      {
+        id: "astroTemplate",
+        name: "Starter template",
+        type: "enum",
+        default: "basics",
+        options: await getFirebaseStudioParams(ref),
+        required: true,
+      },
+    ],
+  };
+  await writeFile(
+    `./.idx-templates/${ref}/idx-template.json`,
+    JSON.stringify(template, null, 2) + "\n",
+    "utf-8",
+  );
+  console.timeEnd(`Updated ${ref}/idx-template.json`);
 }
