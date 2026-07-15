88471b7970e7079b69a3f26ba6379f1a59e840f5
delucis <357379+delucis@users.noreply.github.com>
2026-07-15 10:52:17 +0000

    Apply automatic changes

--- astro.config.ts
+++ astro.config.ts
@@ -1,27 +1,26 @@
-import netlify from "@astrojs/netlify";
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import netlify from '@astrojs/netlify';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 /* https://docs.netlify.com/configure-builds/environment-variables/#read-only-variables */
-const NETLIFY_PREVIEW_SITE =
-  process.env.CONTEXT !== "production" && process.env.DEPLOY_PRIME_URL;
+const NETLIFY_PREVIEW_SITE = process.env.CONTEXT !== 'production' && process.env.DEPLOY_PRIME_URL;
 
 // https://astro.build/config
 export default defineConfig({
-  site: NETLIFY_PREVIEW_SITE || "https://astro.new",
-  prefetch: true,
-  redirects: {
-    "/repro/": "/repro/getting-started",
-  },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
-  adapter: netlify({}),
-  vite: {
-    ssr: {
-      noExternal: ["smartypants"],
-    },
-  },
+	site: NETLIFY_PREVIEW_SITE || 'https://astro.new',
+	prefetch: true,
+	redirects: {
+		'/repro/': '/repro/getting-started',
+	},
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
+	adapter: netlify({}),
+	vite: {
+		ssr: {
+			noExternal: ['smartypants'],
+		},
+	},
 });
